3fe4a1e87e63afa98cb066cf9a050db02fee67f0
Sgeo <sgeoster@gmail.com>
2015-10-14 18:12:54 -0400

    More documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
 
 #![warn(missing_docs)]
 
+//! This crate provides types `Nil` and `Cons<H, T>`, which together allow for creating lists consisting of multiple types.
+//! The types in the list are present in the type of the list, so that `Cons<i32, Cons<i64, Nil>>` contains an `i32` and an `i64`.
+//! If type `T` is present exactly once in an `HList`, it is usually possible to allow the compiler to find the `T` in the `HList` by using the `Find` trait.
+
 /// The empty `HList`.
 pub struct Nil;
 
 /// An `HList` with `H` at position 0, and `T` as the rest of the list.
 pub struct Cons<H, T>(pub H, pub T);
 
+/// A marker trait that `Nil` and `Cons<H, T>` satisfies.
+/// Not currently used to enforce proper hlists, although this may change.
+/// Provides the `push()` method
 pub trait HList: Sized {
     /// Consumes the `HList`, and returns a new HList with `item` at the beginning.
     fn push<N>(self, item: N) -> Cons<N, Self> {
@@ -22,7 +29,7 @@ impl<H, T> HList for Cons<H, T> {}
 ///
 /// `Here` is 0, pointing to the head of the HList.
 ///
-/// Users should normally allow type inference to deal with this type
+/// Users should normally allow type inference to create this type
 #[allow(dead_code)]
 pub enum Here {}
 
@@ -31,7 +38,7 @@ pub enum Here {}
 ///
 /// `There<T>` is 1 + `T`.
 ///
-/// Users should normally allow type inference to deal with this type.
+/// Users should normally allow type inference to create this type.
 #[allow(dead_code)]
 pub struct There<T>(std::marker::PhantomData<T>);
 
@@ -56,8 +63,38 @@ pub struct There<T>(std::marker::PhantomData<T>);
 /// let b: i32 = *list.get(); 
 /// assert!(b == 0);
 /// ```
+///
+/// Functions that need to look up values of a type in an HList given to them should get the index from the call site:
+///
+/// ```rust
+/// fn foo<I, L: Find<i32, I>>(list: &L) -> i32 {
+///     *list.get()
+/// }
+/// let list = Nil.push("foo").push(5i32).push("bar");
+/// assert!(foo(&list) == 5);
+/// ```rust
+/// 
+/// When `foo()` is called, the compiler figures out the appropriate value for `I`.
 pub trait Find<T, I> {
+    /// Retrieves a &T.
+    /// Allows for type inferencing to act like type-directed search
+    /// ```rust
+    /// use hlist::{HList, Nil, Find}
+    ///
+    /// let list = Nil.push(0i32).push(1i64);
+    /// let a: i64 = *list.get();
+    /// assert!(a == 1);
     fn get(&self) -> &T;
+    
+    /// Retrieves a &mut T.
+    /// Allows for type inferencing to act like type-directed search
+    /// ```rust
+    /// use hlist::{HList, Nil, Find}
+    ///
+    /// let mut list = Nil.push(0i32).push(1i64);
+    /// *list.get_mut() = 5i32;
+    /// let a: i32 = *list.get()
+    /// assert!(a == 5);
     fn get_mut(&mut self) -> &mut T;
 }
 
